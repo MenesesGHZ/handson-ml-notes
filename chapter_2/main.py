f23d54a140f421d44471dc832351ae3efe06f198
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-15 18:12:04 -0800

    working on preporcess pipeline

--- chapter_2/main.py
+++ chapter_2/main.py
@@ -3,7 +3,7 @@ from plotter import plot_housing_data
 from sklearn.svm import SVR
 from sklearn.model_selection import RandomizedSearchCV
 from data_loader import fetch_housing_data, load_housing_data 
-
+from preprocess import load_pipeline
 
 def main(download_data):
     # Download data
@@ -12,14 +12,24 @@ def main(download_data):
     
     # Load data
     dataset = load_housing_data()
+    numerical_attr = ["median_income","total_bedrooms","total_rooms","housing_median_age","households","population","longitude","latitude"]
+    categorical_attr = ["ocean_proximity"]
+    response_attr = "median_house_value"
+
+    #Dividing dataset into X,y
+    X = dataset[numerical_attr+categorical_attr]
+    y = dataset[response_attr]
     
     # Plotting data for visualization
-    pairplot_attributes = ["median_house_value", "median_income", "total_rooms","housing_median_age"]
-    plot_housing_data(dataset,pairplot_attributes) 
+  #  pairplot_attributes = ["median_house_value", "median_income", "total_rooms","housing_median_age"]
+  #  plot_housing_data(dataset,pairplot_attributes) 
         
+    # Loading Pipeline to preprocess data
+    pipeline = load_pipeline(numerical_attr,categorical_attr,response_attr)
+    
     # Preprocessing data
-    #pipeline = load_pipeline()
-
+    pipeline.fit(X,y)
+    print(housing_prepared)
 
     # SVM as a regresor model
     #support_vector_machine_reg = SVR()
--- chapter_2/preprocess.py
+++ chapter_2/preprocess.py
@@ -1,7 +1,8 @@
-from sklearn.pipeline import Pipeline
+from scipy import stats
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler, LabelBinarizer
 from sklearn.base import BaseEstimator, TransformerMixin
-
 
 # Select specific attributes among data to apply to them the next transformations in the pipeline 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
@@ -11,13 +12,38 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
+    def transform(self, X):
+        return X[self.attribute_names].values
 
-def load_pipeline(numerical_attributes,categorical_attributes,imputer_strategy="median",scaler_range=(0,1)):
+
+# Select the variables that has the strongest correlation with the response.
+class CorrelationFilter(BaseEstimator, TransformerMixin):
+    def __init__(self,numerical_attr,response_attr,coef_criterium): # no *args or **kargs
+        if response_attr in numerical_attr:
+            numerical_attr.remove(response_attr)
+        self.numerical_attr = numerical_attr 
+        self.response_attr = response_attr
+        self.coef_criterium = coef_criterium
+
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    
+    def transform(self, X, y=None):
+        selected_attr = [self.response_attr]
+        # Selecting features with a spearman correlation higher than `self.coef_criterium`
+        for attr in self.numerical_attr:
+            coef = stats.spearmanr(X[attr],X[self.response_attr])[0]
+            if coef > abs(self.coef_criterium):
+                selected_attr.append(attr)
+        return X[selected_attr]
+
+def load_pipeline(numerical_attributes,categorical_attributes,response_attribute,imputer_strategy="median",scaler_range=(0,1),coef_criterium=0.2):
     # Pipeline for numerical attributes
     numerical_pipeline = Pipeline([
             ("selector", DataFrameSelector(numerical_attributes)),
-            ("imputer",Imputer(strategy=imputer_strategy)),
-            ("scaler",MinMaxScaler(feature_range=scaler_range))
+            ("imputer",SimpleImputer(strategy=imputer_strategy)),
+            ("scaler",MinMaxScaler(feature_range=scaler_range)),
+            ("corr_filter",CorrelationFilter(numerical_attributes,response_attribute,coef_criterium))
             ])
 
     # Pipeline for categorical attributes
